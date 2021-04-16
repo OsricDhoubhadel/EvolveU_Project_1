d393c6efdd30d8c734ae5ab67a08423448da67e6
Osric <dhoubade@ualberta.ca>
2021-04-15 18:56:12 -0600

    Added logic file

---
+++ model/logic.js
@@ -0,0 +1,74 @@
+const available_items =  ["knife","warm_clothes","med_supplies","food"]
+let player_data = {"health":100, "inventory":[]}
+
+function check_inventory(item){
+    if (player_data.inventory.includes(item)){return true}
+    return false}
+
+function random_between(a,b){return Math.floor(a + Math.random() * (b-a))}
+
+function damage_return(a,b,message){
+    const damage = random_between(a,b)
+        player_data.health -= damage
+        if (player_data.health>0) { 
+        console.log(message, damage)
+        console.log(player_data)
+        return player_data}
+        console.log ("GAME OVER")
+}
+
+//Scene 1:
+async function scene_1(item_1, item_2){
+    if (available_items.includes(item_1) && available_items.includes(item_2))
+    {player_data.inventory.push(item_1, item_2)
+    console.log(player_data)
+    return player_data}
+
+    console.log("Invalid_item")    
+}
+
+// Scene2: 0=> don't give food, 1=> give food
+async function scene_2(give_or_no_give){
+    if (check_inventory("food") && give_or_no_give){
+        const index = player_data.inventory.indexOf("food");
+        if (index > -1) {player_data.inventory.splice(index, 1)}
+        player_data.inventory.push("med_supplies")
+        console.log("Food removed from inventory. Neighbour gave you med_supplies in return")
+        return player_data
+    }
+    else if (check_inventory("knife")){
+        console.log("When the neighbour tried to be aggressive, you threatened him with a knife. Both went their own ways unharmed.")
+        return player_data
+    }
+    else {
+        damage_return(20,40,"Fight occurs and you end up taking a small damage of")
+    }
+}
+
+//Scene 3 0=> no fight wolf, 1=> fight
+async function scene_3(fight_or_no_fight){
+    if (fight_or_no_fight && check_inventory("knife")) 
+        {damage_return(20,30,"You had a knife so took a small damage of") } 
+    else if (fight_or_no_fight && !check_inventory("knife")) 
+        {damage_return(60,80,"You didn't have a knife so took a big damage of")}
+    else
+        {damage_return(20,40,"You tried to avoid wolf but the wolf still attacked you. You managed to flee but still took a minor damage of")} 
+    }
+
+async function scene_4(){
+    if (!check_inventory("warm_clothes")) {damage_return(60,70,"You didn't have warm clothes during this cold and took a damage of ")}
+    console.log("You had warm clothes in cold weather and took no damage")
+}
+
+async function scene_5(){
+    if (!check_inventory("knife")) {damage_return(40,50,"You didn't have any weapon during a fight and took a damage of ")}
+    else {damage_return(10,20,"You had a knife and took a modest damage of ")}
+}
+// Testing functions:
+scene_1("knife","warm_clothes")
+scene_2(0)
+scene_3(0)
+scene_4()
+scene_5()
+
+module.exports = {scene_1, scene_2, scene_3, scene_4, scene_5}
--- model/survive.js
+++ model/survive.js
@@ -1,42 +1,43 @@
 const scenes = require('./scenes.js')
 
 scenes.createScene(
-    'Act_I', 
+    'Scene_I', 
     'Home Sweet Home:',
     `Your home is a paradise. While you're enjoying life like any regular day, a wildfire breaks out causing the entire town to go into a state of emergency. You need to abandon your house. The gas station is burnt down and you can only take 2 items with you.`,
-    [{ description: "Not so friendly neighborhood", scene: "Act_II"},]
+    [{ description: "Not so friendly neighborhood", scene: "Scene_II"},]
     )
 
 scenes.createScene(
-    'Act_II',
+    'Scene_II',
     'Not so friendly neighborhood',
     `As you begin to leave home, that your neighbours have no food left and are starving. Out of desperation, they ask you for food.`,
-    [{ description: "Wild Wolf", scene: "Act_III"}]
+    [{ description: "Wild Wolf", scene: "Scene_III"}]
     )
 
 scenes.createScene(
-    'Act_III',
+    'Scene_III',
     'Wild Wolf:',
     `You continue along the path, there is a wolf nearby. Its a dangerous animal that can kill you. However, its fur can be used to keep you warm if you don't have a pair of warm clothes if you can kill it. You can choose to engage in a fight or leave.`,
-    [{ description: "Cold Days Ahead", scene: "Act_IV"}]
+    [{ description: "Cold Days Ahead", scene: "Scene_IV"}]
     )
 
 scenes.createScene(
-    'Act_IV',
+    'Scene_IV',
     'Wild Wolf:',
     `Cold Days Ahead:You continue in your journey. As you keep moving you get hit by a severly cold weather. You check for warm clothes in your inventory`,
-    [{ description: "Nearby town", scene: "Act_V"}]
+    [{ description: "Nearby town", scene: "Scene_V"}]
     )
 
 scenes.createScene(
-    'Act_V', 
+    'Scene_V', 
     'Nearby town:',
     `You finally got through winter and reached the nearby town. But along with you there are many displaced people from your hometown causing a strain on the resources in this new town. The grocery stores are emptied. As a result the town's residents are hostile and has turned into a anarchy. It is a kill or be killed situation.`,
-    [{ description: "You have survived", scene: "Act_VI"}]
+    [{ description: "You have survived", scene: "Scene_VI"}]
     )
 
 scenes.createScene(
-    'Act_VI',
+    'Scene_VI',
     'You have survived:',
     `Despite all the obstacles and adversity you have survived to see another day. Congratulations Survivor! You have made it so far and are looking forward to better days.`
     )
+
